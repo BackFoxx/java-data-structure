d9a79fe1fd745297f0c7e64103f17853419ceaeb
backFox <jorealbusiness@gmail.com>
2022-08-03 02:33:11 +0900

    #1 - 연결 리스트 구현
    값과 다음 노드의 위치를 한 쌍으로 가진 Node를 연결하는 리스트를 구현하고 테스트했다.
    연결 리스트는 배열 리스트에 반해 리스트의 크기가 동적이다. 다만 배열 리스트는 배열의 어떤 위치의 값을 찾아내든 0(1)의 시간이 걸리는 반면 연결 리스트는 무조건 처음부터 출발하여 노드를 찾아야 하므로 0(n)의 시간이 걸린다.

--- src/list/Test.java
+++ src/list/Test.java
@@ -15,5 +15,24 @@ public class Test {
         arrayList.append(700);
         arrayList.remove(1);
         arrayList.removeItem(600);
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(0, 300);
+        linkedList.add(0, 200);
+        linkedList.add(0, 100); //1
+
+        linkedList.append(500);
+        linkedList.append(600); //2
+
+        linkedList.remove(3); //3
+
+        linkedList.add(3, 250);
+        linkedList.add(1, 50);
+        linkedList.add(0, 10); //4
+
+        linkedList.append(700);
+
+        linkedList.remove(1);
+        linkedList.removeItem(600);
     }
 }
